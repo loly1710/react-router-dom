d6779dc0cfa40e89ef68c0da2970421100ce1359
loly1710 <loly15710@gmail.com>
2024-11-11 10:52:21 +0300

    Finish

--- src/main.jsx
+++ src/main.jsx
@@ -2,9 +2,24 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
+// src/main.jsx
+import { BrowserRouter } from 'react-router-dom';
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
+    <BrowserRouter>
+     <App />
+    </BrowserRouter>
+   
   </StrictMode>,
+)
+
+// src/main.jsx
+// Wrap App within <BrowserRouter> to enable routing:
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
+  </React.StrictMode>
 )
